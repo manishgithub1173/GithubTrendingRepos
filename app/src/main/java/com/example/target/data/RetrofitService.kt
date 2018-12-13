11bddd30af2a3f4cf8e98761b8dcdd657987aebd
Manish Kumar <manish@urbanladder.com>
2018-12-13 19:41:17 +0530

    Added API for getting trending users

--- app/src/main/java/com/example/target/data/RetrofitService.kt
+++ app/src/main/java/com/example/target/data/RetrofitService.kt
@@ -1,3 +1,14 @@
 package com.example.target.data
 
-interface RetrofitService
+import io.reactivex.Observable
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RetrofitService {
+    @GET("developers/")
+    fun getWeatherDetails(
+        @Query("language") city: String,
+        @Query("since") key: String
+    ): Observable<Response<List<User>>>
+}
