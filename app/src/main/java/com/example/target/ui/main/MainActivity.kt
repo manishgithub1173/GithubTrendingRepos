b342f765c8b751d98334fda01e21aae1038cde5a
Manish Kumar <manish@urbanladder.com>
2018-12-13 18:51:45 +0530

    Added Main activity

---
+++ app/src/main/java/com/example/target/ui/main/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.target.ui.main
+
+import android.Manifest
+import android.os.Bundle
+import com.example.target.R
+import dagger.android.AndroidInjection
+import dagger.android.support.DaggerAppCompatActivity
+
+class MainActivity : DaggerAppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
